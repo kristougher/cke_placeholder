ef4ac5f3de94de45dc62cfd7b48432085e12c35a
Kristoffer Arrild <ka@peytz.dk>
2017-09-20 11:29:03 +0200

    WIP

--- modules/ckep_embed_external/src/Plugin/CkePlaceholderLibrary/CkepEmbedExternalLibrary.php
+++ modules/ckep_embed_external/src/Plugin/CkePlaceholderLibrary/CkepEmbedExternalLibrary.php
@@ -3,19 +3,20 @@
 namespace Drupal\ckep_embed_external\Plugin\CkePlaceholderLibrary;
 
 use Drupal\cke_placeholder\CkePlaceholderLibraryBase;
+use Alb\OEmbed;
 
 /**
  * Provides a CkepFileEntity plugin.
  *
  * @CkePlaceholderLibrary(
- *   id = "ckep_file_entity",
- *   description = @Translation("Ckep File Entity plugin for CKE Placeholder library."),
- *   pane_title = @Translation("Media library"),
- *   list_wrapper_id = "ckep_file_entity-media-list",
- *   module = "ckep_file_entity"
+ *   id = "ckep_embed_external",
+ *   description = @Translation("CKE Placeholder library plugin to embed external content."),
+ *   pane_title = @Translation("Embed external"),
+ *   list_wrapper_id = "ckep_embed_external",
+ *   module = "ckep_embed_external"
  * )
  */
-class CkepEmbedExternal extends CkePlaceholderLibraryBase {
+class CkepEmbedExternalLibrary extends CkePlaceholderLibraryBase {
 
   /**
    * {@inheritdoc}
@@ -39,13 +40,15 @@ class CkepEmbedExternal extends CkePlaceholderLibraryBase {
       '#value' => t('Fetch content'),
       '#ajax' => [
         'wrapper' => 'ckep-embed-external-preview',
-        'callback' => 'Drupal\ckep_embed_external\Plugin\CkePlaceholderLibrary::getExternalContent',
+        'callback' => 'Drupal\ckep_embed_external\Plugin\CkePlaceholderLibrary\CkepEmbedExternalLibrary::getExternalContent',
       ],
 
     ];
+    $discovery = new OEmbed\Discovery;
+    $provider = $discovery->discover('http://vimeo.com/31423544');
 
     $form['ckep_embed_external']['preview'] = [
-      '#markup' => '<div id="ckep-embed-external-preview"></div>',
+      '#markup' => '<div id="ckep-embed-external-preview"></div>' . $provider->getTitle(),
     ];
 
     return $form;
@@ -54,14 +57,18 @@ class CkepEmbedExternal extends CkePlaceholderLibraryBase {
   /**
    * AJAX callback for processing the submitted URL.
    */
-  public static getExternalContent($form, $form_state) {
+  public static function getExternalContent($form, $form_state) {
+    $response = OEmbed\Simple::request('https://vimeo.com/164619178');
+    if ($title = $response->getTitle()) {
+      return ['#markup' => $title];
+    }
     return ['#markup' => print_r($form_state->getValues(), 1)];
   }
 
   /**
    * Get the content that should be available in the media library.
    *
-   * @param null|string $type
+   * @param FormStateInterface $form_state
    *   The entity type to fetch.
    *
    * @return array
